b10af85ba69bbb317954b7b418c38e299722f716
florayym <yuyim@126.com>
2020-05-13 04:32:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# create-ics
+
+Create your own .ics file for UC San Diego Calendar. You can add that to your calendar app, such as Google Calendar and Outlook.
+
+Implement with:
+
+- [cheerio](https://www.npmjs.com/package/cheerio)
+- [ical-generator](https://www.npmjs.com/package/ical-generator)
+
+# Quick Start
+
+```shell
+# install dependencies
+>npm install
+# start the script
+>npm start
+```
---
+++ index.js
@@ -0,0 +1,100 @@
+/**
+ * Using Node's support for ECMAScript modules.
+ */
+'use strict';
+
+import request from 'request';
+import cheerio from 'cheerio';
+import ical from 'ical-generator';
+
+const calendar_url = 'https://students.ucsd.edu/academics/enroll/calendar20-21.html';
+
+request(calendar_url, function (error, response, body) {
+
+  console.log('statusCode:', response && response.statusCode);
+  if (error) {
+    console.error('error', error);
+    return;
+  }
+
+  const $ = cheerio.load(body);
+
+  const calendar_name = 'UC San Diego ' + $('title').text();
+  const cal = ical({
+    prodId: {
+      company: 'students.ucsd.edu',
+      product: 'university-cal-generator',
+      language: 'EN'
+    },
+    name: calendar_name
+  });
+
+  //const res = $('div[class=body-text]').find('a').attr('href');
+
+  let header = [];
+
+  $('th').each(function () {
+    header.push($(this).text());
+  })
+
+  $('tr').each(function () {
+
+    if ($(this).children().length == 5) {
+
+      let description, description_html;
+
+      $(this).find('tr > td').each(function (index) {
+
+        if (index === 0) { // Description
+
+          description = $(this).text();
+          description = description.replace(/&quot;/g, '\"');
+          description = description.replace(/\r?\n|\r/g, '');
+
+          description_html = $(this).html();
+          description_html = description_html.replace(/&#xA0\;/g, '');
+
+        } else if (index != 4) { // add event except for summer quarter
+
+          let date_str = $(this).text();
+
+          if (date_str === 'N/A') {
+            return;
+          }
+
+          let year = '20' + header[index].substr(header[index].length - 2);
+
+          date_str = date_str.split('-');
+          let startDate_str = date_str[0].split('/');
+          let startDate = new Date(year, parseInt(startDate_str[0], 10) - 1, parseInt(startDate_str[1], 10), 11, 59);
+
+          const event = cal.createEvent({
+            start: startDate,
+            allDay: true,
+            //timezone: 'America/Los_Angeles',
+            //floating: true,
+            htmlDescription: description_html,
+            organizer: {
+              name: 'UC San Diego',
+              email: 'q5yu@cs.ucsd.edu'
+            },
+            summary: header[index] + ': ' + description,
+            createAlarm: {
+              type: 'display',
+              trigger: 900 // 15 minutes before event
+            }
+          });
+
+          if (date_str.indexOf('-') != -1) {
+            let endDate_str = date_str[1].split('/');
+            let endDate = new Date(year, parseInt(endDate_str[0], 10) - 1, parseInt(endDate_str[1], 10), 11, 59);
+            event.end(endDate);
+          }
+        }
+      });
+    }
+  });
+
+  cal.saveSync(calendar_name + '.ics');
+
+});
