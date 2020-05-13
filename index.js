aaf9040ddf2180dbed12ad71ca4fa4232eda0433
florayym <yuyim@126.com>
2020-05-13 12:41:55 +0800

    year fix

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ request(calendar_url, function (error, response, body) {
     name: calendar_name
   });
 
-  //cal.alarms([])
-
   //const res = $('div[class=body-text]').find('a').attr('href');
 
   let header = [];
@@ -56,7 +54,7 @@ request(calendar_url, function (error, response, body) {
           description_html = $(this).html();
           description_html = description_html.replace(/&#xA0\;/g, '');
 
-        } else if (index != 4) { // add event except for summer quarter
+        } else if (index != 4) { // Add event except for summer quarter
 
           let date_str = $(this).text();
 
@@ -64,18 +62,25 @@ request(calendar_url, function (error, response, body) {
             return;
           }
 
-          let year = '20' + header[index].substr(header[index].length - 2);
+          const quarter = header[index];
 
           date_str = date_str.split('-');
-          let startDate_str = date_str[0].split('/');
-          let startDate = new Date(year, parseInt(startDate_str[0], 10) - 1, parseInt(startDate_str[1], 10), 23, 59);
+          const startDate_str = date_str[0].split('/');
+
+          const month = parseInt(startDate_str[0], 10) - 1;
+          let year = '20' + quarter.substr(quarter.length - 2);
+
+          if (quarter.startsWith('Winter') && month >= 5) year = '2020';
+          else if (quarter.startsWith('Spring') && month >= 8) year = '2020';
+
+          let startDate = new Date(year, month, parseInt(startDate_str[1], 10), 23, 59);
 
           const event = cal.createEvent({
             start: startDate,
             allDay: true,
             //timezone: 'America/Los_Angeles',
             //floating: true,
-            summary: header[index] + ' ' + description,
+            summary: quarter + ' ' + description,
             description: description_html,
             organizer: {
               name: 'UC San Diego',
@@ -89,7 +94,7 @@ request(calendar_url, function (error, response, body) {
 
           if (date_str.length === 2) {
             let endDate_str = date_str[1].split('/');
-            let endDate = new Date(year, parseInt(endDate_str[0], 10) - 1, parseInt(endDate_str[1], 10), 23, 59);
+            let endDate = new Date(year, month, parseInt(endDate_str[1], 10), 23, 59);
             event.end(endDate);
           }
         }
